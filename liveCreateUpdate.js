3947b6b659a3a1d0b6b616953cde5c7fff53f4de
John Olson <john.olson3@gmail.com>
2018-06-12 08:46:49 -0700

    add snippets

---
+++ liveCreateUpdate.js
@@ -0,0 +1,31 @@
+﻿
+
+function vm() {
+    var self = this;
+    var formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+    });
+
+    self.description = ko.observable();
+
+    self.retailPrice = ko.observable();
+    self.retailPriceDisplay = ko.pureComputed(function () {
+        return parseCurrency(self.retailPrice());
+    });
+
+    self.salePrice = ko.observable();
+    self.salePriceDisplay = ko.pureComputed(function () {
+        return parseCurrency(self.salePrice());
+    });
+
+    self.imagePath = ko.observable();
+
+    function parseCurrency(num) {
+        if (num === undefined) return formatter.format(0);
+        return formatter.format(num);
+    }
+}
+
+ko.applyBindings(new vm());
